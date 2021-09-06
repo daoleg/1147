02d17b2d9e74ebe6a784de3193062dbcf93b4601
daoleg <90193038+daoleg@users.noreply.github.com>
2021-09-06 15:33:02 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Фома";
+        int age =75;
+        String city = "Кострома";
+        System.out.println(name+", " +age+" лет, "+city);
+    }
+}
