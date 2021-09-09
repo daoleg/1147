a21be1d7f8f2b712106512e6ade29ea3708df917
daoleg <90193038+daoleg@users.noreply.github.com>
2021-09-09 13:15:24 +0300

    Create Main.java

---
+++ task_2.1/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String word1 = "Мама";
+        String word2 = "Мыла";
+        String word3 = "Раму";
+        System.out.println(word1+word2+word3);
+        System.out.println(word1+word3+word2);
+        System.out.println(word2+word1+word3);
+        System.out.println(word2+word3+word1);
+        System.out.println(word3+word1+word2);
+        System.out.println(word3+word2+word1);
+        
+    }
+}
